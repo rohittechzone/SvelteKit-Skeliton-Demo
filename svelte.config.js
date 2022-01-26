411e83d06c182a118917208e5c7fc17fbb43b498
Rohit <63205012+rohittechzone@users.noreply.github.com>
2022-01-26 19:01:22 +0530

    Add files via upload

---
+++ svelte.config.js
@@ -0,0 +1,18 @@
+const netlify = require('@sveltejs/adapter-netlify')
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	kit: {
+		adapter: netlify(),
+
+		// hydrate the <div id="svelte"> element in src/app.html
+		target: '#svelte',
+
+		// Override http methods in the Todo forms
+		methodOverride: {
+			allowed: ['PATCH', 'DELETE']
+		}
+	}
+};
+
+export default config;
